1d35fe63bd10b43a4f0c03c83a299bf76b7b79d2
Luiz Fernando <lfaires@gmail.com>
2021-07-12 14:40:02 -0300

    get items route try-catch

--- src/app.js
+++ src/app.js
@@ -8,9 +8,15 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/items', async (req,res) => {
-    const result = await connection.query(`SELECT * FROM items`)
-    const items = result.rows
+    try{
+        const result = await connection.query(`SELECT * FROM items`)
+        const items = result.rows
 
-    res.send(items)
+        res.send(items)
+        
+    } catch (error){
+        console.log(error)
+        res.sendStatus(500)
+    }
 })
 export default app;
